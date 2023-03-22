e0e7b8a5bfaa5b1edcb82c431bd0fba00066ce0d
vkolachi <36785379+vkolachi@users.noreply.github.com>
2023-03-22 23:24:06 +0530

    Add files via upload

---
+++ intro/learnFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class learnFrame {
+    public static void main(String[] args){
+    JFrame frame=new JFrame();
+    frame.getContentPane().setBackground(Color.CYAN);
+    frame.setTitle("sdfdf");
+    frame.setLocation(200,200);
+    frame.setSize(800,600);
+    frame.setBackground(Color.CYAN);
+
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+}}
